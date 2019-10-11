0c56ea1b41cca702048d52b4bc4088a75bbf586e
Tucker <tmiles7@vols.utk.edu>
2019-10-10 23:24:29 -0400

    removed worthless commented out blocks

--- YardScrapped.js
+++ YardScrapped.js
@@ -52,20 +52,10 @@ function lightsCommand(arguments, receivedMessage) {
     }
 
 }
-
-
-
 /*
 client.on('ready', () => {
     var generalChannel = client.channels.get("317112475814461440") // Replace with known channel ID
     generalChannel.send("this is a test and my name is tucker")  
 })
 */
-
-// Get your bot's secret token from:
-// https://discordapp.com/developers/applications/
-// Click on your application -> Bot -> Token -> "Click to Reveal Token"
-//bot_secret_token = "NjI1MTU4NDI4OTk3ODQ1MDIy.XYbgyA.e3nr9_ovKKwcu_KyZ3fXZ1jeqf4"
-
-
 client.login(bot_secret_token)
